31c09255372624377259c1b464ede5ca7cc3fac0
casthewiz <cschaufe@kent.edu>
2016-02-01 23:56:22 -0500

    Graphly initial commit.

---
+++ graphly.py
@@ -0,0 +1,30 @@
+
+class node:
+    index = -1
+    children = []
+
+    def __init__(self, index, children):
+        self.index = index
+        self.children = children
+
+    def set(self, child):
+        self.children.push(child)
+
+    def output(self):
+        print(str(self.index) + " " + str(self.children))
+
+# Notes about graphly
+
+class graph:
+    # bucket is the container for all nodes
+    bucket = []
+    size = 0
+
+    # takes in size
+    def __init__(self, qArray):
+        self.bucket = [node(ind, i) for ind, i in enumerate(qArray)]
+        self.size = len(self.bucket)
+
+    def output(self):
+        for i in self.bucket:
+            i.output()
---
+++ main.py
@@ -0,0 +1,5 @@
+from graphly import *
+
+x = [[1,2],[0,2],[1,0]]
+y = graph(x)
+y.output()
