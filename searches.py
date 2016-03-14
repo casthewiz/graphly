9900bfe4f05ac0f078221f38916f8ccf2c7f601c
Cassidy Schaufele <cschaufe@kent.edu>
2016-03-14 13:39:14 -0400

    Added searches.py from www.github.com/casthewiz/BFS-and-DFS

---
+++ searches.py
@@ -0,0 +1,29 @@
+def dfs(g, v):
+    # g is a graph object
+    # v is start node
+    visited = set()
+    stack = [v]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            #do relevant function call here
+            #add to stack vertexes in visited node
+            stack.extend(set(g[vertex]).difference(visited))
+    return visited
+
+
+def bfs(g, v):
+    # g is a graph object
+    # v is start node
+    visited = set()
+    queue = [v]
+    while stack:
+        vertex = queue.pop(0)
+        # and just like that, our list works like a queue!
+        if vertex not in visited:
+            visited.add(vertex)
+            #do relevant function call here
+            #add to queue vertexes in visited node
+            queue.extend(set(g[vertex]).difference(visited))
+    return visited
