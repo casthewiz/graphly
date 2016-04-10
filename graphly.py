1705fc9e0b71a134c01e5fe4dec8316e767b938d
casthewiz <cschaufe@kent.edu>
2016-04-09 21:50:59 -0400

    Finished random generation, added testcase

--- graphly.py
+++ graphly.py
@@ -1,3 +1,4 @@
+from random import *
 
 class node:
     index = -1
@@ -8,7 +9,7 @@ class node:
         self.children = children
 
     def set(self, child):
-        self.children.push(child)
+        self.children.append(child)
 
     def output(self):
         print(str(self.index) + " " + str(self.children))
@@ -29,8 +30,22 @@ class graph:
         for i in self.bucket:
             i.output()
 
-    def generate(self):
-
+    #size determines the number of nodes in a given graph
+    #connectivity is the average factor of nodes connected - e.g .10 is a node will connect to 10% of the graph.
+    #variation is the degree of entropy for connections - e.g. .50 means that the connectivity for a given node will vary within 50% of it's given value
+    #WARNING - connectivity or variation should not exceed 1.0 !
+    def generate(self, size = 10, connectivity=.10, variation=.50):
+        g = list()
+        for i in range(0, size):
+            sub = []
+            connectionFlux = (variation * connectivity)
+            connectionFactor = round(uniform(0 - connectionFlux, connectionFlux), 2) + connectivity
+            connectionNumber = size * connectivity
+            for j in range(0, int(connectionNumber)):
+                sub.append(randrange(0, size))
+            g.append(sub)
+        newGraph = graph(g)
+        self.bucket = newGraph.bucket
         return 0
 
     def fExport(self):
--- main.py
+++ main.py
@@ -3,11 +3,5 @@ from searches import *
 
 x = [[1,2],[0,2],[1,0],[4,1],[2,0]]
 y = graph(x)
+y.generate(100)
 y.output()
-
-z = dfs(x, 0)
-print(z)
-z = dfs(x, 4)
-print(z)
-z = dfs(x, 3)
-print(z)
